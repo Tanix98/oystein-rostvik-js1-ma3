904a4e01a74724bd03fb469286dc1d6c0d89140b
Tanix <91118560+Tanix98@users.noreply.github.com>
2022-02-15 12:11:56 +0100

    Add files via upload

---
+++ js/question-2.js
@@ -0,0 +1,8 @@
+const key = "0da38fff62ac41cda60963a363a82df7";
+const api = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
+
+fetch(api){
+    for(let i = 0; i < 8; i++){
+
+    }
+}
