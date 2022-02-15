6387f79ebbc8132a5beb058bcdce67f017995dca
Tanix <91118560+Tanix98@users.noreply.github.com>
2022-02-15 15:29:52 +0100

    Add files via upload

--- js/question-2.js
+++ js/question-2.js
@@ -1,8 +1,18 @@
 const key = "0da38fff62ac41cda60963a363a82df7";
-const api = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
 
-fetch(api){
+const dataContainer = document.querySelector(".data");
+
+async function callApi(){
+    const response = await fetch(url);
+    const data = await response.json();
+    const profileData = data.results;
+
+    dataContainer.innerHTML = "";
+    
     for(let i = 0; i < 8; i++){
-
+        dataContainer.innerHTML += `<div class="game-info"><p class="title-text">Game title:</p>${profileData[i].name} <p class="rating-text">Rating:</p> ${profileData[i].rating} <p class="tag-amount-text">Number of tags:</p> ${profileData[i].tags.length}</div>`;
     }
 }
+
+callApi();
